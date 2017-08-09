934c72365a5498a056e2025fad78f706b03a3234
Andrew Duthie <andrew@andrewduthie.com>
2017-08-09 18:52:07 -0400

    Components: Shift focus into popover when opened

--- components/popover/index.js
+++ components/popover/index.js
@@ -8,6 +8,7 @@ import { isEqual, pickBy } from 'lodash';
  * WordPress dependencies
  */
 import { createPortal, Component } from '@wordpress/element';
+import { focus } from '@wordpress/utils';
 
 /**
  * Internal dependencies
@@ -26,6 +27,7 @@ export class Popover extends Component {
 	constructor() {
 		super( ...arguments );
 
+		this.focus = this.focus.bind( this );
 		this.bindNode = this.bindNode.bind( this );
 		this.setOffset = this.setOffset.bind( this );
 		this.throttledSetOffset = this.throttledSetOffset.bind( this );
@@ -60,6 +62,10 @@ export class Popover extends Component {
 		const { isOpen: prevIsOpen, position: prevPosition } = prevProps;
 		if ( isOpen !== prevIsOpen ) {
 			this.toggleWindowEvents( isOpen );
+
+			if ( isOpen ) {
+				this.focus();
+			}
 		}
 
 		if ( ! isOpen ) {
@@ -85,6 +91,13 @@ export class Popover extends Component {
 		window.cancelAnimationFrame( this.rafHandle );
 		window[ handler ]( 'resize', this.throttledSetOffset );
 		window[ handler ]( 'scroll', this.throttledSetOffset );
+	}
+
+	focus() {
+		const firstFocusable = focus.findFocusable( this.nodes.content );
+		if ( firstFocusable ) {
+			firstFocusable.focus();
+		}
 	}
 
 	throttledSetOffset() {
